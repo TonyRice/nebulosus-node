56ea40f228a6b69f4d860b812f0de92d36724261
Tony Rice <tony@tonyrice.me>
2018-01-02 12:47:12 -0600

    cleaning up uuid.js

--- uuid.js
+++ uuid.js
@@ -1,9 +1,4 @@
 // TODO replace with more secure random.
-function makeUUID() {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
-        .replace(/[xy]/g, function (a, b) {
-            return b = Math.random() * 16, (a == "y" ? b & 3 | 8 : b | 0).toString(16)
-        })
-}
+function makeUUID() {return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (a, b) {return b = Math.random() * 16, (a == "y" ? b & 3 | 8 : b | 0).toString(16)})}
 
 module.exports = makeUUID;
